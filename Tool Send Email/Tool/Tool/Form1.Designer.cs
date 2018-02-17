3b06559a5df1a5f48d5557dc25935d6d298075e2
AJackTi <dtrong97vn@gmail.com>
2018-02-18 00:16:04 +0700

    Update Project
    Update Project

---
+++ Tool Send Email/Tool/Tool/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace Tool
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPath = new System.Windows.Forms.Label();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnSendSlow = new System.Windows.Forms.Button();
+            this.btnSendFast = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnAbout = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.cbxListFile = new System.Windows.Forms.ComboBox();
+            this.btnFileAttach = new System.Windows.Forms.Button();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblPath
+            // 
+            this.lblPath.Location = new System.Drawing.Point(123, 9);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(463, 26);
+            this.lblPath.TabIndex = 0;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(12, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(105, 23);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Open File";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // txtContent
+            // 
+            this.txtContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtContent.Location = new System.Drawing.Point(3, 16);
+            this.txtContent.Multiline = true;
+            this.txtContent.Name = "txtContent";
+            this.txtContent.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtContent.Size = new System.Drawing.Size(583, 150);
+            this.txtContent.TabIndex = 3;
+            this.txtContent.MouseClick += new System.Windows.Forms.MouseEventHandler(this.txtContent_MouseClick);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnSendSlow
+            // 
+            this.btnSendSlow.Location = new System.Drawing.Point(15, 19);
+            this.btnSendSlow.Name = "btnSendSlow";
+            this.btnSendSlow.Size = new System.Drawing.Size(100, 26);
+            this.btnSendSlow.TabIndex = 4;
+            this.btnSendSlow.Text = "Send Slow";
+            this.btnSendSlow.UseVisualStyleBackColor = true;
+            this.btnSendSlow.Click += new System.EventHandler(this.btnSendSlow_Click);
+            // 
+            // btnSendFast
+            // 
+            this.btnSendFast.Location = new System.Drawing.Point(172, 19);
+            this.btnSendFast.Name = "btnSendFast";
+            this.btnSendFast.Size = new System.Drawing.Size(100, 26);
+            this.btnSendFast.TabIndex = 5;
+            this.btnSendFast.Text = "Send Fast";
+            this.btnSendFast.UseVisualStyleBackColor = true;
+            this.btnSendFast.Click += new System.EventHandler(this.btnSendFast_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(486, 19);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(100, 26);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnAbout
+            // 
+            this.btnAbout.Location = new System.Drawing.Point(329, 19);
+            this.btnAbout.Name = "btnAbout";
+            this.btnAbout.Size = new System.Drawing.Size(100, 26);
+            this.btnAbout.TabIndex = 7;
+            this.btnAbout.Text = "About";
+            this.btnAbout.UseVisualStyleBackColor = true;
+            this.btnAbout.Click += new System.EventHandler(this.btnAbout_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnAbout);
+            this.groupBox1.Controls.Add(this.btnSendSlow);
+            this.groupBox1.Controls.Add(this.btnSendFast);
+            this.groupBox1.Controls.Add(this.btnExit);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox1.Location = new System.Drawing.Point(0, 320);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(598, 58);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Function";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.groupBox4);
+            this.groupBox2.Controls.Add(this.txtTitle);
+            this.groupBox2.Controls.Add(this.groupBox3);
+            this.groupBox2.Location = new System.Drawing.Point(0, 38);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(598, 275);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Content Email";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(2, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Title";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.cbxListFile);
+            this.groupBox4.Controls.Add(this.btnFileAttach);
+            this.groupBox4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox4.Location = new System.Drawing.Point(3, 213);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(592, 59);
+            this.groupBox4.TabIndex = 5;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Attachment";
+            // 
+            // cbxListFile
+            // 
+            this.cbxListFile.FormattingEnabled = true;
+            this.cbxListFile.Location = new System.Drawing.Point(117, 26);
+            this.cbxListFile.Name = "cbxListFile";
+            this.cbxListFile.Size = new System.Drawing.Size(267, 21);
+            this.cbxListFile.TabIndex = 2;
+            // 
+            // btnFileAttach
+            // 
+            this.btnFileAttach.Location = new System.Drawing.Point(6, 24);
+            this.btnFileAttach.Name = "btnFileAttach";
+            this.btnFileAttach.Size = new System.Drawing.Size(105, 23);
+            this.btnFileAttach.TabIndex = 0;
+            this.btnFileAttach.Text = "Open File Attach";
+            this.btnFileAttach.UseVisualStyleBackColor = true;
+            this.btnFileAttach.Click += new System.EventHandler(this.btnFileAttach_Click);
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(35, 19);
+            this.txtTitle.Multiline = true;
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(557, 20);
+            this.txtTitle.TabIndex = 4;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.txtContent);
+            this.groupBox3.Location = new System.Drawing.Point(3, 38);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(589, 169);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Content";
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(598, 378);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.lblPath);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "Send Email";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.TextBox txtContent;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnSendSlow;
+        private System.Windows.Forms.Button btnSendFast;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnAbout;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button btnFileAttach;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.ComboBox cbxListFile;
+    }
+}
+
---
+++ Tool Send Email/Tool/Tool/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net.Mail;
+using System.Net;
+using System.Threading;
+using System.Net.Mime;
+
+namespace Tool
+{
+    public partial class frmMain : Form
+    {
+        public bool Flag = true;
+        public static readonly string[] arrFileDontInclude = { ".ADE", ".ADP", ".BAT", ".CHM", ".CMD", ".COM", ".CPL", ".EXE", ".HTA", ".INS", ".ISP", ".JAR", ".JS", ".JSE", ".LIB", ".LNK", ".MDE", ".MSC", ".MSI", ".MSP", ".MST", ".NSH", "PIF", ".SCR", ".SCT", ".SHB", ".SYS", ".VB", ".VBE", ".VBS", ".VXD", ".WSC", ".WSF", ".WSH" };
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                lblPath.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(txtContent.Text))
+                txtContent.Text = "Enter text here...";
+        }
+
+        private void txtContent_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (Flag)
+            {
+                txtContent.Clear();
+                Flag = false;
+            }
+        }
+
+        private List<string> readMailContent(string link)
+        {
+            List<string> lstResults = new List<string>();
+            foreach (string line in File.ReadAllLines(link))
+            {
+                lstResults.Add(line);
+            }
+            return lstResults;
+        }
+
+        private void sendMail(int milisecond, string content, string title)
+        {
+            foreach (var item in readMailContent(openFileDialog1.FileName))
+            {
+                string[] results = item.Split(',');
+                foreach(var items in results) // items = email
+                {
+                    if(IsValid(items))
+                    {
+                        var fromAddress = new MailAddress("kellytrong112@gmail.com", "");
+                        var toAddress = new MailAddress(items, "");
+                        const string fromPassword = "trong0924707771";
+                        string subject = title;
+                        string body = content.ToString(); // txtContent
+
+                        var smtp = new SmtpClient
+                        {
+                            Host = "smtp.gmail.com",
+                            Port = 587,
+                            EnableSsl = true,
+                            DeliveryMethod = SmtpDeliveryMethod.Network,
+                            UseDefaultCredentials = false,
+                            Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+                        };
+                        using (var message = new MailMessage(fromAddress, toAddress)
+                        {
+                            Subject = subject,
+                            Body = body
+                        })
+                        {
+                            if(this.cbxListFile.Items.Count > 0)
+                            {
+                                for (int i = 0; i < this.cbxListFile.Items.Count; i++) // each file
+                                {
+                                    bool flagCheck = true;
+                                    for (int j = 0; j < arrFileDontInclude.Length; j++)
+                                    {
+                                        if (this.cbxListFile.Items[i].ToString().EndsWith(arrFileDontInclude[j].ToString()))
+                                        {
+                                            flagCheck = true;
+                                            break;
+                                        }
+                                    }
+                                    if (flagCheck)
+                                    {
+                                        Attachment attachment = new Attachment(this.cbxListFile.Items[i].ToString(), MediaTypeNames.Application.Octet);
+                                        ContentDisposition disposition = attachment.ContentDisposition;
+                                        disposition.CreationDate = File.GetCreationTime(this.cbxListFile.Items[i].ToString());
+                                        disposition.ModificationDate = File.GetLastWriteTime(this.cbxListFile.Items[i].ToString());
+                                        disposition.ReadDate = File.GetLastAccessTime(this.cbxListFile.Items[i].ToString());
+                                        disposition.FileName = Path.GetFileName(this.cbxListFile.Items[i].ToString());
+                                        disposition.Size = new FileInfo(this.cbxListFile.Items[i].ToString()).Length;
+                                        disposition.DispositionType = DispositionTypeNames.Attachment;
+                                        message.Attachments.Add(attachment);
+                                        flagCheck = false;
+                                    }
+                                }
+
+                            }
+                            Thread.Sleep(milisecond);
+                            smtp.Send(message);
+                        }
+                    }
+                    else // email not true
+                    {
+
+                    }
+                }
+            }
+            
+        }
+
+        public bool IsValid(string emailaddress)
+        {
+            try
+            {
+                MailAddress m = new MailAddress(emailaddress);
+
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+
+        private void btnAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("AJackTi designed\n" +
+                "My linkedin: https://www.linkedin.com/in/ajackti/\n");
+        }
+
+        private void btnSendSlow_Click(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+            sendMail(500, txtContent.Text, txtTitle.Text);
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnFileAttach_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog2.ShowDialog() == DialogResult.OK)
+            {
+                cbxListFile.Items.Add(openFileDialog2.FileName);
+            }
+        }
+
+        private void btnSendFast_Click(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+            sendMail(100, txtContent.Text, txtTitle.Text);
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Do you want to exit?", "Exit?", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) != System.Windows.Forms.DialogResult.OK)
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
